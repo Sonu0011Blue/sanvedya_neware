a78960a56c038f504f4ac4f1d965495c872c7e89
Sonu0011Blue <sanvedyaneware11@gmail.com>
2026-01-03 18:49:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("contact-form");
 
+  if (!form) return;
+
   form.addEventListener("submit", e => {
     e.preventDefault();
 
